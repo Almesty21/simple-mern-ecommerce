61398e62465b264892aa6fd18babda2cc44a7c26
Almesty21 <mihretabgirma21@gmail.com>
2022-03-29 13:43:12 +0300

    First Commit

---
+++ client/src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="justify-content-center d-flex">
+        <div className="card-name">
+          <img
+            alt="visa"
+            src="https://upload.wikimedia.org/wikipedia/commons/4/41/Visa_Logo.png"
+          />
+        </div>
+        <div className="card-name">
+          <img
+            alt="paypal"
+            src="https://pbs.twimg.com/media/EfTZlEnWAAMn1lX.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
